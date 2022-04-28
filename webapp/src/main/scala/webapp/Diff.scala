8297d28831683da19682eee7688c7a77056c364f
Felix Dietze <felix.dietze@rwth-aachen.de>
2022-04-28 08:55:24 -0600

    Refactor Diff

--- webapp/src/main/scala/webapp/Diff.scala
+++ webapp/src/main/scala/webapp/Diff.scala
@@ -7,18 +7,26 @@ import typings.diff.mod._
 // https://github.com/kpdecker/jsdiff
 
 object Diff {
+
   def apply(original: String, result: String) = {
     p(
-      diffWordsWithSpace(original, result)
-        .map(part =>
-          span(
-            part.value,
-            cls := (if (part.added.getOrElse(false)) "text-green-500 dark:text-green-400"
-                    else if (part.removed.getOrElse(false)) "text-red-500 dark:text-red-400"
-                    else ""),
-          ),
+      diffWordsWithSpace(original, result).map { part =>
+        val isAdded   = part.added.getOrElse(false)
+        val isRemoved = part.removed.getOrElse(false)
+        span(
+          // showControlChars(part.value),
+          part.value,
+          if (isAdded) (cls := "text-green-500 dark:text-green-400")
+          else if (isRemoved) (cls := "text-red-500 dark:text-red-400")
+          else VDomModifier.empty,
         ),
+      },
     )
 
   }
+
+  def showControlChars(str: String) = str
+    .replaceAll("\n", "¶\n")
+    .replaceAll(" ", "·")
+
 }
