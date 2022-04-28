9bc23b06191e86a169f7d6951f8d1ebe80595a16
Felix Dietze <felix.dietze@rwth-aachen.de>
2022-04-28 09:21:40 -0600

    Use diff-patch-match for showing diffs

--- webapp/src/main/scala/webapp/Diff.scala
+++ webapp/src/main/scala/webapp/Diff.scala
@@ -2,20 +2,24 @@ package webapp
 
 import outwatch._
 import outwatch.dsl._
-import typings.diff.mod._
-
-// https://github.com/kpdecker/jsdiff
+import typings.diffMatchPatch.mod._
 
 object Diff {
+  val dmp = new diffMatchPatch();
 
   def apply(original: String, result: String) = {
+    // https://github.com/google/diff-match-patch/wiki/API#diff_maintext1-text2--diffs
+    val diff = dmp.diff_main(original, result)
+    dmp.diff_cleanupSemantic(diff)
+
     p(
-      diffWordsWithSpace(original, result).map { part =>
-        val isAdded   = part.added.getOrElse(false)
-        val isRemoved = part.removed.getOrElse(false)
+      diff.map { part =>
+        val str       = part(1)
+        val isAdded   = part(0) == 1
+        val isRemoved = part(0) == -1
         span(
-          // showControlChars(part.value),
-          part.value,
+          // showControlChars(str),
+          str,
           if (isAdded) (cls := "text-green-500 dark:text-green-400")
           else if (isRemoved) (cls := "text-red-500 dark:text-red-400")
           else VDomModifier.empty,
